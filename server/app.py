15b92a158cb5bf7879741a0a2a757317fb74980f
Sabrina Freifeld <sfrei.1996@gmail.com>
2024-04-26 11:50:47 -0600

    python

--- server/app.py
+++ server/app.py
@@ -17,6 +17,8 @@ db.init_app(app)
 api = Api(app)
 
 
+
+
 class Plants(Resource):
 
     def get(self):
@@ -35,20 +37,47 @@ class Plants(Resource):
         db.session.add(new_plant)
         db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
+    
 
-api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(plant, key, value)
+
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
+api.add_resource(Plants, '/plants')
+
 
 
 if __name__ == '__main__':
